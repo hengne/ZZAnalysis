39318f54ac62031137d3a009e686b5e662e2d472
Giacomo Ortona <gortona@cern.ch>
2014-07-08 13:53:29 +0200

    very first test, just compiling for now

--- AnalysisStep/interface/PhotonFwd.h
+++
@@ -1,7 +0,0 @@
-#include <AnalysisDataFormats/CMGTools/interface/Photon.h>
-#include <vector>
-
-typedef cmg::Photon            Photon;
-typedef edm::Ptr<Photon>       PhotonPtr;
-typedef std::vector<PhotonPtr> PhotonPtrVector;
-
